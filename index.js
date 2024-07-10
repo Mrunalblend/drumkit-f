4bcfba5e93a41e377cd4ccc63cc5a0bd9aae9d1f
Mrunalblend <136045721+Mrunalblend@users.noreply.github.com>
2024-07-10 13:14:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+// Function to play the soumd
+function playMusic(parameter){
+    switch(parameter){
+        case "w":
+            var tom1 = new Audio('tom-1.mp3');
+            tom1.play();
+         break;
+        case "a" :
+            var tom2 = new Audio('tom-2.mp3');
+            tom2.play();
+         break;
+        case "s" :
+            var tom3 = new Audio('tom-3.mp3');
+            tom3.play();
+         break;
+        case "d" :
+            var tom4 = new Audio('tom-4.mp3');
+            tom4.play();
+         break;
+        case "j" :
+            var snare = new Audio('snare.mp3');
+            snare.play();
+         break;
+         case "k" :
+            var crash = new Audio('crash.mp3');
+            crash.play();
+         break;
+         case "l" :
+            var kick = new Audio('kick-bass.mp3');
+            kick.play();
+         break;
+        default : console.log(parameter);  
+    }
+}
+
+// Function for button animation
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed"); 
+    }, 100)
+}
+
+// Detecting button press
+var drumLength = document.querySelectorAll(".drum").length;
+var i=0;
+while(i<drumLength){
+document.querySelectorAll(".drum")[i].addEventListener("click" , function (){
+
+    var buttonInnerHTML = this.innerHTML;
+    playMusic(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+}
+)
+i++;
+}
+// Detecting keypress
+document.addEventListener("keypress" , function(event){
+    playMusic(event.key);
+    buttonAnimation(event.key);
+})
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+//     audio.play();
